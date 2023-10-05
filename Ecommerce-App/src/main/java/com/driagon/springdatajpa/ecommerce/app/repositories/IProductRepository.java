a69b22379d5a0b55d28cdef1e9e81f7fae867965
Adrian Gonzalez <g.adrian.gonzalez.90@gmail.com>
2023-10-04 21:58:23 -0600

    Added finder methods part 1

--- Ecommerce-App/src/main/java/com/driagon/springdatajpa/ecommerce/app/repositories/IProductRepository.java
+++ Ecommerce-App/src/main/java/com/driagon/springdatajpa/ecommerce/app/repositories/IProductRepository.java
@@ -3,5 +3,24 @@ package com.driagon.springdatajpa.ecommerce.app.repositories;
 import com.driagon.springdatajpa.ecommerce.app.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 public interface IProductRepository extends JpaRepository<Product, Long> {
+
+
+    Product findByName(String name);
+
+    Optional<Product> findById(Long id);
+
+    List<Product> findByNameOrDescription(String name, String description);
+
+    List<Product> findByNameAndDescription(String name, String description);
+
+    Product findDistinctByName(String name);
+
+    List<Product> findByPriceGreaterThan(BigDecimal price);
+
+    List<Product> findByPriceLessThan(BigDecimal price);
 }
---
+++ Ecommerce-App/src/test/java/com/driagon/springdatajpa/ecommerce/app/repositories/QueryMethodsTest.java
@@ -0,0 +1,83 @@
+package com.driagon.springdatajpa.ecommerce.app.repositories;
+
+import com.driagon.springdatajpa.ecommerce.app.models.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@SpringBootTest
+public class QueryMethodsTest {
+
+    @Autowired
+    private IProductRepository productRepository;
+
+    @Test
+    void findByNameMethod() {
+        Product product = this.productRepository.findByName("Product 1");
+
+        System.out.println("product.getId() = " + product.getId());
+        System.out.println("product.getName() = " + product.getName());
+        System.out.println("product.getDescription() = " + product.getDescription());
+    }
+
+    @Test
+    void findByIdMethod() {
+        Product product = this.productRepository.findById(10L).get();
+
+        System.out.println("product.getId() = " + product.getId());
+        System.out.println("product.getName() = " + product.getName());
+        System.out.println("product.getDescription() = " + product.getDescription());
+    }
+
+    @Test
+    void findByNameOrDescriptionMethod() {
+        List<Product> products = this.productRepository.findByNameOrDescription("Product 1", "Product 1 Description");
+
+        products.forEach(p -> {
+            System.out.println("p.getId() = " + p.getId());
+            System.out.println("p.getName() = " + p.getName());
+        });
+    }
+
+    @Test
+    void findByNameAndDescriptionMethod() {
+        List<Product> products = this.productRepository.findByNameAndDescription("Product 1", "Product 1 Description");
+
+        products.forEach(p -> {
+            System.out.println("p.getId() = " + p.getId());
+            System.out.println("p.getName() = " + p.getName());
+        });
+    }
+
+    @Test
+    void findDistinctByNameMethod() {
+        Product product = this.productRepository.findDistinctByName("Product 1");
+
+        System.out.println("product.getId() = " + product.getId());
+        System.out.println("product.getName() = " + product.getName());
+        System.out.println("product.getDescription() = " + product.getDescription());
+    }
+
+    @Test
+    void findByPriceGreaterThanMethod() {
+        List<Product> products = this.productRepository.findByPriceGreaterThan(new BigDecimal(100));
+
+        products.forEach(p -> {
+            System.out.println("p.getId() = " + p.getId());
+            System.out.println("p.getName() = " + p.getName());
+        });
+    }
+
+    @Test
+    void findByPriceLessThanMethod() {
+        List<Product> products = this.productRepository.findByPriceLessThan(new BigDecimal(200));
+
+        products.forEach(p -> {
+            System.out.println("p.getId() = " + p.getId());
+            System.out.println("p.getName() = " + p.getName());
+        });
+    }
+}
