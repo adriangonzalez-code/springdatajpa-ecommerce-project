7ddbadc5fd3f915d16a11d89621e96d18c730813
Adrian Gonzalez <g.adrian.gonzalez.90@gmail.com>
2023-11-28 20:38:00 -0600

    Completed tests method

--- Testing-JPA/src/main/java/com/driagon/springboot/testing/app/repositories/IEmployeeRepository.java
+++ Testing-JPA/src/main/java/com/driagon/springboot/testing/app/repositories/IEmployeeRepository.java
@@ -2,6 +2,28 @@ package com.driagon.springboot.testing.app.repositories;
 
 import com.driagon.springboot.testing.app.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
+
+    // Define custom query using JPQL with index params
+    @Query("select e from Employee e where e.firstName = ?1 and e.lastName = ?2")
+    Employee findByJPQL(String firstName, String lastName);
+
+    // Define custom query using JPQL with named params
+    @Query("select e from Employee e where e.firstName = :firstName and e.lastName = :lastName")
+    Employee findByJPQLNamedParams(@Param("firstName") String firstName, @Param("lastName") String lastName);
+
+    // Define custom query using Native SQL with index params
+    @Query(value = "select * from employees e where e.first_name = ?1 and e.last_name = ?2", nativeQuery = true)
+    Employee findByNativeSQL(String firstName, String lastName);
+
+    // Define custom query using Native SQL with named params
+    @Query(value = "select * from employees e where e.first_name = :firstName and e.last_name = :lastName", nativeQuery = true)
+    Employee findByNativeSQLNamed(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
--- Testing-JPA/src/test/java/com/driagon/springboot/testing/app/repositories/EmployeeRepositoryTests.java
+++ Testing-JPA/src/test/java/com/driagon/springboot/testing/app/repositories/EmployeeRepositoryTests.java
@@ -1,12 +1,14 @@
 package com.driagon.springboot.testing.app.repositories;
 
 import com.driagon.springboot.testing.app.models.Employee;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,18 +18,23 @@ public class EmployeeRepositoryTests {
     @Autowired
     private IEmployeeRepository repository;
 
-    // Unit test for save employee operation
+    private Employee employee;
 
+    @BeforeEach
+    void setup() {
+        this.employee = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Fadatare")
+                .email("ramesh@mail.com")
+                .build();
+    }
+
+    // Unit test for save employee operation
     @Test
     @DisplayName("JUnit test for save employee operation")
     void givenEmployee_whenSave_thenReturnSavedEmployee() {
 
         // Given - Precondition or setup
-        Employee employee = Employee.builder()
-                .firstName("Ramesh")
-                .lastName("Ramesh")
-                .email("ramesh@mail.com")
-                .build();
 
         // When - Action or behaviour that are going test
         Employee savedEmployee = this.repository.save(employee);
@@ -41,11 +48,6 @@ public class EmployeeRepositoryTests {
     @Test
     void givenEmployeesList_whenFindAll_thenEmployeesList() {
         // Given - Precondition or setup
-        Employee employee1 = Employee.builder()
-                .firstName("Ramesh")
-                .lastName("Ramesh")
-                .email("ramesh@mail.com")
-                .build();
 
         Employee employee2 = Employee.builder()
                 .firstName("John")
@@ -53,7 +55,7 @@ public class EmployeeRepositoryTests {
                 .email("john.cena@mail.com")
                 .build();
 
-        this.repository.save(employee1);
+        this.repository.save(employee);
         this.repository.save(employee2);
 
         // When - Action or the behaviour that we are going test
@@ -68,12 +70,6 @@ public class EmployeeRepositoryTests {
     @Test
     void givenEmployee_whenFindById_thenReturnEmployeeObject() {
         // Given - Precondition or setup
-        Employee employee = Employee.builder()
-                .firstName("Ramesh")
-                .lastName("Ramesh")
-                .email("ramesh@mail.com")
-                .build();
-
         this.repository.save(employee);
 
         // When - Action or the behaviour that we are going test
@@ -81,5 +77,109 @@ public class EmployeeRepositoryTests {
 
         // Then - Verify the output
         assertThat(employeeDB).isNotNull();
+    }
+
+    @DisplayName("Junit test for get Employee by email operation")
+    @Test
+    void givenEmployeeEmail_whenFindByEmail_thenReturnEmployeeObject() {
+        // Given - Precondition or setup
+        this.repository.save(employee);
+
+        // When - Action or the behaviour that we are going test
+        Employee employeeDB = this.repository.findByEmail(employee.getEmail()).get();
+
+        // Then - Verify the output
+        assertThat(employeeDB).isNotNull();
+    }
+
+    @DisplayName("Junit test for update employee operation")
+    @Test
+    void givenEmployeeObject_whenUpdateEmployee_thenReturnUpdatedEmployee() {
+        // Given - Precondition or setup
+        this.repository.save(employee);
+
+        // When - Action or the behaviour that we are going test
+        Employee savedEmployee = this.repository.findById(employee.getId()).get();
+        savedEmployee.setEmail("ram@mail.com");
+        savedEmployee.setFirstName("Ram");
+
+        Employee updatedEmployee = this.repository.save(savedEmployee);
+
+        // Then - Verify the output
+        assertThat(updatedEmployee.getEmail()).isEqualTo("ram@mail.com");
+        assertThat(updatedEmployee.getFirstName()).isEqualTo("Ram");
+    }
+
+    @DisplayName("Junit test for delete Employee operation")
+    @Test
+    void givenEmployeeObject_whenDelete_thenRemoveEmployee() {
+        // Given - Precondition or setup
+        this.repository.save(employee);
+
+        // When - Action or the behaviour that we are going test
+        this.repository.delete(employee);
+
+        Optional<Employee> employeeOptional = this.repository.findById(employee.getId());
+
+        // Then - Verify the output
+        assertThat(employeeOptional).isEmpty();
+    }
+
+    @DisplayName("Junit test for custom query using JPQL with index")
+    @Test
+    void givenFirstNameAndLastName_whenFindByJPQL_thenReturnEmployeeObject() {
+        // Given - Precondition or setup
+        this.repository.save(employee);
+
+        String firstName = "Ramesh";
+        String lastName = "Fadatare";
+
+        // When - Action or the behaviour that we are going test
+        Employee savedEmployee = this.repository.findByJPQL(firstName, lastName);
+
+        // Then - Verify the output
+        assertThat(savedEmployee).isNotNull();
+    }
+
+    @DisplayName("Junit test for custom query using JPQL with Named params")
+    @Test
+    void givenFirstNameAndLastName_whenFindByJPQLNamedParams_thenReturnEmployeeObject() {
+        // Given - Precondition or setup
+        this.repository.save(employee);
+
+        String firstName = "Ramesh";
+        String lastName = "Fadatare";
+
+        // When - Action or the behaviour that we are going test
+        Employee savedEmployee = this.repository.findByJPQLNamedParams(firstName, lastName);
+
+        // Then - Verify the output
+        assertThat(savedEmployee).isNotNull();
+    }
+
+    @DisplayName("Junit test for custom query using Native SQL with index")
+    @Test
+    void givenFirstNameAndLastName_whenFindByNativeSQL_thenReturnEmployeeObject() {
+        // Given - Precondition or setup
+        this.repository.save(employee);
+
+        // When - Action or the behaviour that we are going test
+        Employee savedEmployee = this.repository.findByNativeSQL(employee.getFirstName(), employee.getLastName());
+
+        // Then - Verify the output
+        assertThat(savedEmployee).isNotNull();
+    }
+
+    @DisplayName("Junit test for custom query using Native SQL with named params")
+    @Test
+    void givenFirstNameAndLastName_whenFindByNativeSQLNamedParams_thenReturnEmployeeObject() {
+        // Given - Precondition or setup
+        this.repository.save(employee);
+
+        // When - Action or the behaviour that we are going test
+        Employee savedEmployee = this.repository.findByNativeSQLNamed(employee.getFirstName(), employee.getLastName());
+
+        // Then - Verify the output
+        assertThat(savedEmployee).isNotNull();
     }
 }
